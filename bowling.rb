601eec52f2f2585d32c9a9be0a84976efca19322
c1505 <coreym247@gmail.com>
2016-08-16 14:48:32 +0000

    added frames

--- bowling.rb
+++ bowling.rb
@@ -9,10 +9,19 @@ class Game
   def initialize
     @score = 0
     @all_rolls = []
+    @frames = []
   end
   
   def roll(num)
     @all_rolls << num
+
+    if @frames.length == 10
+      @frames.last << num
+    elsif @frames.length == 0 || @frames.last[0] == 10 || @frames.last.length == 2
+      @frames << [num] #add to next one
+    else 
+      @frames.last << num #add to current one
+    end
   end
   
   # the last frame needs to be a special case in a different way.  each frame
@@ -21,9 +30,9 @@ class Game
     new_frames = []
     @all_rolls.each_with_index do |score, index|
       counter = 0
-      if frame_count == index + 1
+      # if frame_count == index + 1
         
-      elsif strike?(score)
+      if strike?(score)
         arr = [score, (@all_rolls[index + 1]), (@all_rolls[index + 2]) ]
         counter = arr.compact.reduce(:+)
       elsif spare?(score, index)
@@ -33,12 +42,16 @@ class Game
       end
       new_frames << counter
     end
+    print frame_count
     new_frames.reduce(:+)
+    
+    
   end
   
   def frame_count
     count = 0
     @all_rolls.each do |roll|
+      return count if count == 10
       if roll == 10
         count += 1
       else
