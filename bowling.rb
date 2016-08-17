5953e9975b576176b3a55770493c647648664a47
c1505 <coreym247@gmail.com>
2016-08-17 08:46:56 -0400

    refactor extract method and improve naming.  flog total 148. largest 48.8.

--- bowling.rb
+++ bowling.rb
@@ -7,7 +7,6 @@ class Game
   attr_accessor :score
   attr_reader :frames
   
-  
   def initialize
     @score = 0
     @frames = []
@@ -15,12 +14,10 @@ class Game
   
   def roll(num)
     raise RuntimeError, 'not a valid roll' unless valid_roll?(num)
-    raise RuntimeError if game_over?
+    raise RuntimeError, 'game is over.  no more rolls remain' if game_over?
     if @frames.length == 10
       @frames.last << num
-      if @frames.last[2] && (@frames.last[1] < 10)
-        raise RuntimeError if @frames.last[1] + @frames.last[2] > 10
-      end
+      exceeds_pins?
     elsif @frames.length == 0 || @frames.last[0] == 10 || @frames.last.length == 2
       @frames << [num] #add to next one
     else
@@ -29,26 +26,36 @@ class Game
     end
   end
   
+  def exceeds_pins?
+    # if @frames.length == 10 && !(strike? || spare?)
+    if @frames.last[2] && (@frames.last[1] < 10)
+      raise RuntimeError if @frames.last[1] + @frames.last[2] > 10
+    end
+  end
+
   def score
     raise RuntimeError, 'game is not over' unless game_over?
-    new_frames = []
+    scored_frames = []
     @frames.each_with_index do |frame, index|
       if index == 9
-        counter = frame.reduce(:+)
+        counter = frame_total(frame)
       elsif strike?(frame)
-        arr = [frame[0], (@frames[index + 1][0]), (@frames[index + 1][1] || @frames[index + 2][0]) ]
+        arr = [frame_total(frame), (@frames[index + 1][0]), (@frames[index + 1][1] || @frames[index + 2][0]) ]
         counter = arr.compact.reduce(:+)
       elsif spare?(frame)
-        counter = frame[0] + frame[1] + @frames[index + 1][0]
+        counter = frame_total(frame) + @frames[index + 1][0]
       else
-        counter = frame[0] + frame[1]
+        counter = frame_total(frame)
       end
-      new_frames << counter
+      scored_frames << counter
     end
-    new_frames.reduce(:+)
+    scored_frames.reduce(:+)
   end
-  
-    
+
+  def frame_total(frame)
+    frame.reduce(:+)
+  end
+
   def spare?(frame) 
     (frame[0] + frame[1]) == 10
   end
@@ -71,19 +78,9 @@ class Game
     result
   end
   
-# @game.frames.length >= 10 && @game.frames.last.length == 2
-  
   def valid_roll?(num)
     num >= 0 && num <= 10 
   end
     
 
-
-# 105.0: flog total
-#     13.1: flog/method average
-
-#     54.9: Game#score                       bowling.rb:54
-#     24.2: Game#roll                        bowling.rb:15
-
-  
 end
