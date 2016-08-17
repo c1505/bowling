50a8e1e783e7ee2deb2ebb5addfde1ba4000be44
c1505 <coreym247@gmail.com>
2016-08-17 16:59:54 +0000

    refactored score method.  flog total 148.3.  largest method 33.6

--- bowling.rb
+++ bowling.rb
@@ -13,43 +13,47 @@ class Game
   end
   
   def roll(num)
-    raise RuntimeError, 'not a valid roll' unless valid_roll?(num)
+    # in a real game, i would decide the end of the frame after the roll.  here i am deciding at the beginning of the next roll
+    raise RuntimeError, 'not a valid roll' unless valid_roll?(num) #should valid roll include idea of too many pins?
     raise RuntimeError, 'game is over.  no more rolls remain' if game_over?
+    exceeds_pins?(num)
+  
     if @frames.length == 10
       @frames.last << num
-      exceeds_pins?
-    elsif @frames.length == 0 || @frames.last[0] == 10 || @frames.last.length == 2
+    elsif @frames.length == 0 || strike?(@frames.last) || @frames.last.length == 2
       @frames << [num] #add to next one
     else
-      raise RuntimeError if (num + @frames.last[0]) > 10
       @frames.last << num #add to current one
     end
   end
   
-  def exceeds_pins?
-    # if @frames.length == 10 && !(strike? || spare?)
-    if @frames.last[2] && (@frames.last[1] < 10)
-      raise RuntimeError if @frames.last[1] + @frames.last[2] > 10
+  def exceeds_pins?(num=0)
+    return unless @frames.last && @frames.last[0]
+    if @frames.length == 10 && @frames.last[1] && (num < 10) 
+      raise RuntimeError, 'last frame exceeds pin count' if num + @frames.last[1] > 10
+    elsif (num + @frames.last[0]) > 10 && !strike?(@frames.last)
+      raise RuntimeError, 'normal frame exceeds pin count'
     end
   end
 
   def score
     raise RuntimeError, 'game is not over' unless game_over?
-    scored_frames = []
-    @frames.each_with_index do |frame, index|
+    @frames.map.with_index do |frame, index|
       if index == 9
-        counter = frame_total(frame)
+        frame_total(frame)
       elsif strike?(frame)
-        arr = [frame_total(frame), (@frames[index + 1][0]), (@frames[index + 1][1] || @frames[index + 2][0]) ]
-        counter = arr.compact.reduce(:+)
+        frame_total(frame) + strike_score(index)
       elsif spare?(frame)
-        counter = frame_total(frame) + @frames[index + 1][0]
+        frame_total(frame) + @frames[index + 1][0]
       else
-        counter = frame_total(frame)
+        frame_total(frame)
       end
-      scored_frames << counter
-    end
-    scored_frames.reduce(:+)
+    end.reduce(:+)
+  end
+  
+  def strike_score(index)
+    arr = [(@frames[index + 1][0]), (@frames[index + 1][1] || @frames[index + 2][0]) ]
+    arr.compact.reduce(:+)
   end
 
   def frame_total(frame)
